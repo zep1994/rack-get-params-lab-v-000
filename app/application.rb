2130d7913a8be89a5c7dab9f78babd54be891fa3
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-06-27 00:10:17 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -24,6 +24,16 @@ class Application
     elsif req.path.match(/add/)
  
       search_term = req.params["q"]
+      
+     if @@items.include?(search_term)
+        resp.write "#{search_term} is one of our items"
+      else
+        resp.write "Couldn't find #{search_term}"
+      end
+ 
+    else
+      resp.write "Path Not Found"
+    end
  
     resp.finish
   end
